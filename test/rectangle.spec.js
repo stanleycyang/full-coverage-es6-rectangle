faea19284b6f84cd5fa6127827c385fe0555f590
stanleycyang <stanley@stanleycyang.com>
2015-09-05 15:16:27 -0700

    100 percent coverage

--- test/rectangle.spec.js
+++ test/rectangle.spec.js
@@ -6,6 +6,26 @@ chai.should();
 import Rectangle from '../rectangle';
 
 describe('Rectangle', () => {
+  describe('#constructor()', () => {
+    it('requires two numerical arguments', () => {
+      () => {
+        new Rectangle();
+      }.should.throw(Error);
+
+      () => {
+        new Rectangle(1.0);
+      }.should.throw(Error);
+
+      () => {
+        new Rectangle('foo', 'bar');
+      }.should.throw(Error);
+
+      () => {
+        new Rectangle(5, 7);
+      }.should.not.throw(Error);
+
+    });
+  });
   describe('#width', () => {
     let rectangle;
 
@@ -25,6 +45,65 @@ describe('Rectangle', () => {
     it('only accepts numerical values', () => {
       () => {
         rectangle.width = 'foo';
+      }.should.throw(Error);
+    });
+  });
+
+  describe('#height', () => {
+    let rectangle;
+
+    beforeEach(() => {
+      rectangle = new Rectangle(10, 20);
+    });
+
+    it('returns the height', () => {
+      rectangle.height.should.equal(20);
+    });
+
+    it('can be changed', () => {
+      rectangle.height = 30;
+      rectangle.height.should.equal(30);
+    });
+
+    it('only accepts numerical values', () => {
+      () => {
+        rectangle.height = 'foo';
+      }.should.throw(Error);
+    });
+  });
+
+  describe('#area', () => {
+    let rectangle;
+
+    beforeEach(() => {
+      rectangle = new Rectangle(10, 20);
+    });
+
+    it('returns the area', () => {
+      rectangle.area.should.equal(200);
+    });
+
+    it('cannot be change', () => {
+      () => {
+        rectangle.area = 1000;
+      }.should.throw(Error);
+    });
+  });
+
+  describe('#circumference', () => {
+    let rectangle;
+
+    beforeEach(() => {
+      rectangle = new Rectangle(10, 20);
+    });
+
+    it('returns the circumference', () => {
+      rectangle.circumference.should.equal(60);
+    });
+
+    it('cannot be changed', () => {
+      () => {
+        rectangle.circumference = 1000;
       }.should.throw(Error);
     });
   });
